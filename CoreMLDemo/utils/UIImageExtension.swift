f374af34808c9ce96e0dab797f913101972a0fea
hugues.stephano.telolahy <hugues.stephano.telolahy@devoteam.com>
2018-03-06 10:37:01 +0100

    add new model

---
+++ CoreMLDemo/utils/UIImageExtension.swift
@@ -0,0 +1,13 @@
+//
+//  UIImageExtension.swift
+//  CoreMLDemo
+//
+//  Created by Stéphano TELOLAHY on 06/03/2018.
+//  Copyright © 2018 Hugues Stéphano TELOLAHY. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    
+}
